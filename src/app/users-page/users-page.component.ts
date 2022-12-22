6b2a7cbd16c193ad5aff8b522062cbe3ab2e19e7
colombusAH <--add>
2022-12-22 18:45:45 +0200

    paging

--- src/app/users-page/users-page.component.ts
+++ src/app/users-page/users-page.component.ts
@@ -10,10 +10,23 @@ export class UsersPageComponent implements OnInit {
 
   constructor(private usersService: UsersService) { }
   users: any[] = [];
+  currentPage: number = 1;
 
   async ngOnInit(): Promise<void> {
-    this.users = await this.usersService.fetchCharacters();
+    this.users = await this.usersService.fetchCharacters(this.currentPage);
     console.log(this.users);
+  }
+
+  async nextPage() {
+    this.currentPage += 1;
+    this.users = await this.usersService.fetchCharacters(this.currentPage);
+  }
+  async prevPage() {
+    if (this.currentPage === 1) {
+      return;
+    }
+    this.currentPage -= 1;
+    this.users = await this.usersService.fetchCharacters(this.currentPage);
   }
 
 } 
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -10,8 +10,8 @@ export class UsersService {
   constructor(private http: HttpClient) { }
 
 
-  async fetchCharacters() {
-    const charactersRes = await lastValueFrom(this.http.get("https://rickandmortyapi.com/api/character")) as any;
+  async fetchCharacters(page: number = 0) {
+    const charactersRes = await lastValueFrom(this.http.get(`https://rickandmortyapi.com/api/character?page=${page}`)) as any;
     console.log(charactersRes);
     return charactersRes.results;
   }
